a69dc68a093a460ff8f2d5dec1f032c37cd61cf4
nofar.barabes@melio.com <nofar.baranes@melio.com>
2024-01-26 14:48:43 +0200

    App Welcome Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="content">
+        <h1>Welcome to my project App</h1>
       </header>
+      <section className="content">
+        <h2>Nice to meet you! 👋</h2>
+        <p>I am Nofar Baranes and I am a Lead Front-End developer based in Israel.</p>
+        <p>I have 10 years of experience in planning, developing, and executing multiple projects in the latest technologies.<br/>
+        My expertise includes coding with ES6/TypeScript, React (+ Hooks), Redux, MobX, Sass, styled-components, and more.<br/>
+        I am a motivated, determined, and independent quick learner of new technologies with high standards of professionalism.</p>
+      </section>
+      <section className="content">
+        <h2>About the project</h2>
+        <p>This project includes frontend challenges, such as games and tools, aimed at improving my skills in React and TypeScript.</p>
+        <p>The project also include a Storybook with the common components I have created for the challenges to ensuring consistency in language across the components featured in the pages and games of this project is a priority. I strive to produce a uniform language that maintains a cohesive and seamless experience throughout. </p>
+      </section>
     </div>
   );
 }
